d4248cdeb37dd5705b03206ccaa066d53e98d10f
Marius Andrian <marius.atj@gmail.com>
2025-05-21 08:08:09 +0800

    add chapter 1.10

---
+++ chapter1.10.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+// Illustrating external variables.
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+
+// Be careful that 'getline' will conflict with getline in stdio.h and stop compilation.
+int Getline(void);
+void copy(void);
+
+int main()
+{
+    int len, max;
+    extern char line[];
+    extern char longest[];
+
+    max = 0;
+    while ((len = Getline()) > 0)
+        if (len > max)
+        {
+            max = len;
+            copy();
+        }
+    if (max > 0)
+    {
+        printf("%s", longest);
+    }
+}
+
+// We modify line in-place and return the length of the line.
+int Getline(void)
+{
+    extern char line[];
+    int c, i;
+
+    for (i = 0; i < MAXLINE - 1 && ((c = getchar()) != EOF) && c != '\n'; ++i)
+        line[i] = c;
+    if (c == '\n')
+    {
+        line[i] = c;
+        ++i;
+    }
+    line[i] = '\0';
+    return i;
+}
+
+void copy()
+{
+    int i;
+    extern char line[], longest[];
+
+    i = 0;
+    while ((longest[i] = line[i]) != '\0')
+        ++i;
+}
